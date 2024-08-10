1e9315a36ef3b99e7913a2d1bbe5cb66f40cc33c
sahilsrivastava25 <sahilsrivastava827@gmail.com>
2024-08-10 06:32:56 +0530

    new commit

---
+++ File Hider/src/main/java/views/Welcome.java
@@ -0,0 +1,62 @@
+package views;
+
+import dao.UserDAO;
+import model.User;
+import service.UserService;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Welcome {
+    public void welcomeScreen() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Welcome to the app");
+        System.out.println("Press 1 to login");
+        System.out.println("Press 2 to signup");
+        System.out.println("Press 0 to exit");
+        int choice = 0;
+        try {
+            choice = Integer.parseInt(br.readLine()); //takes input
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        switch (choice) {
+            case 1 -> login();
+            case 2 -> signUp();
+            case 0 -> System.exit(0);
+        }
+    }
+
+    private void login() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter email");
+        String email = sc.nextLine();
+        try {
+            if(UserDAO.isExists(email)) {
+                   new UserView(email).home();
+           }else {
+                System.out.println("User not found");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    private void signUp() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name");
+        String name = sc.nextLine();
+        System.out.println("Enter email");
+        String email = sc.nextLine();
+        User user = new User(name, email);
+        int response = UserService.saveUser(user);
+        switch (response) {
+            case 1 -> System.out.println("User registered");
+            case 0 -> System.out.println("User already exists");
+        }
+    }
+}
